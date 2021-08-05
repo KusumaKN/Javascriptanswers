6817a5b4236ea5ca86e46a199e5e9525caf5d242
KusumaKN <88476758+KusumaKN@users.noreply.github.com>
2021-08-05 17:37:45 +0530

    Update question9.js

--- question9.js
+++ question9.js
@@ -2,7 +2,7 @@ const gimli =
 { name: "Gimli",
 race: "dwarf",
 weapon: "axe",
-greet: function() { return `Hi, my name is ${this.name}!`; }, };
-gimli.greet();
+greet: function() { return `Hi, my name is ${this.name}!`;},};
+alert(gimli.greet());
 
 //output:Hi, my name is Gimli!
