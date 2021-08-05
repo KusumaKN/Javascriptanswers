ce5bb7803647fd3c95e9d8f79eb0a54a7e8bac09
KusumaKN <88476758+KusumaKN@users.noreply.github.com>
2021-08-05 12:54:19 +0530

    Create question10.js

---
+++ question10.js
@@ -0,0 +1,7 @@
+function Hero(name, level) {
+    this.name = name;
+    this.level = level;
+}
+
+
+//A constructor function is initialized with a number of parameters, which would be assigned as properties of this, referring to the function itself.
